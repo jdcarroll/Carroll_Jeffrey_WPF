1966d19d116d032d95de3b7278d1b5dc7d179c8d
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 00:02:14 -0400

    adding comments

--- assignment_expressions/industry_specific/js/main.js
+++ assignment_expressions/industry_specific/js/main.js
@@ -1,6 +1,6 @@
 var imgSize = prompt("How many pixels are in you img?"); // I am asking the user for the size of the image
-var screenWidth = window.innerWidth;
-var screenHeight = window.innerHeight;
+var screenWidth = window.innerWidth;// I am using this to calculate the window size
+var screenHeight = window.innerHeight;//
 var screenSize = screenHeight * screenWidth;
 var imgFit = Math.round(screenSize / imgSize);
 console.log("your Screen Size equals: " + screenSize + "px and you can fit " + imgFit + " copies of your image into " +

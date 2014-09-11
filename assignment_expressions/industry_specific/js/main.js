e6fb43f4e3981b10c42414964e43c394d2e1ecbc
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 00:22:41 -0400

    correcting statement

--- assignment_expressions/industry_specific/js/main.js
+++ assignment_expressions/industry_specific/js/main.js
@@ -4,14 +4,14 @@ var imgWidth = Number(prompt("What is the width of your image?")); // I am askin
 
 var screenWidth = window.innerWidth;// I am using this to calculate the window size.
 
-var across = screenWidth / imgWidth;// I am attempting to calculate how many times the image goes across the screen
+var across = Math.floor(screenWidth / imgWidth);// I am attempting to calculate how many times the image goes across the screen
 
 var screenHeight = window.innerHeight;// This the height of the window not the screen size of the monitor.
 
-var down = screenHeight / imgHeight;// Im attempting to calculate how many times the image goes down the screen
+var down = Math.floor(screenHeight / imgHeight);// Im attempting to calculate how many times the image goes down the screen
 
 var screenSize = screenHeight * screenWidth; // This is where the actual calculation happens.
 
 console.log("your Screen Size equals: " + screenSize + "px and you can fit the image across the " +
-    "screen " + across + " amount of times.  You can fit the img down the screen " + down + " amount of times");
+    "screen " + across + " times.  You can fit the img down the screen " + down + " times");
     /* I am echoing out the results of the calculation to the console.*/

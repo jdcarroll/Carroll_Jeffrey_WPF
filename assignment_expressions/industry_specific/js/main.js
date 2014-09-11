361b3cbfe7bc7dcfb50c7a889ef397a611ea1316
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 00:03:09 -0400

    calculate height

--- assignment_expressions/industry_specific/js/main.js
+++ assignment_expressions/industry_specific/js/main.js
@@ -1,6 +1,6 @@
 var imgSize = prompt("How many pixels are in you img?"); // I am asking the user for the size of the image
 var screenWidth = window.innerWidth;// I am using this to calculate the window size
-var screenHeight = window.innerHeight;//
+var screenHeight = window.innerHeight;// This the height of the window not the screen size of the monitor
 var screenSize = screenHeight * screenWidth;
 var imgFit = Math.round(screenSize / imgSize);
 console.log("your Screen Size equals: " + screenSize + "px and you can fit " + imgFit + " copies of your image into " +

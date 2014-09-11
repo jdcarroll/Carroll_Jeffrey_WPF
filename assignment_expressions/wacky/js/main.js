68cc6a36860240f9e63c1ee6ae5657b27c06fad9
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 00:33:14 -0400

    here is my wacky one

---
+++ assignment_expressions/wacky/js/main.js
@@ -0,0 +1,8 @@
+var punch = prompt("You are in a fight how many times do you want to punch your opponent?");
+var sure = prompt("Are you sure?");
+    if (sure == yes || sure == y){
+        prompt("You are a where you are fighting Mike Tyson Right?" + "I cant believe you tried to punch Mike Tyson" + punch +
+        " many times.");
+    } else {
+        alert("your not as dumb because your opponent is Mike Tyson but you loose anyway");
+    }

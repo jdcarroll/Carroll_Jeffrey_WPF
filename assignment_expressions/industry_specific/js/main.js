f3114f05254ab55728e4ec0f86200f6e0c80cc90
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 00:05:24 -0400

    comment the calculation of a variable

--- assignment_expressions/industry_specific/js/main.js
+++ assignment_expressions/industry_specific/js/main.js
@@ -1,7 +1,12 @@
-var imgSize = prompt("How many pixels are in you img?"); // I am asking the user for the size of the image
-var screenWidth = window.innerWidth;// I am using this to calculate the window size
-var screenHeight = window.innerHeight;// This the height of the window not the screen size of the monitor
+var imgSize = prompt("How many pixels are in you img?"); // I am asking the user for the size of the image.
+
+var screenWidth = window.innerWidth;// I am using this to calculate the window size.
+
+var screenHeight = window.innerHeight;// This the height of the window not the screen size of the monitor.
+
 var screenSize = screenHeight * screenWidth; // This is where the actual calculation happens.
-var imgFit = Math.round(screenSize / imgSize);
+
+var imgFit = Math.round(screenSize / imgSize); // calculate the size of th image and round to the closed whole number.
+
 console.log("your Screen Size equals: " + screenSize + "px and you can fit " + imgFit + " copies of your image into " +
     "your current window.");

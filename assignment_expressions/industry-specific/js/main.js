d822c367766fce63a3aecc154cc0d028fb60d81a
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-10 23:07:26 -0400

    fist step

---
+++ assignment_expressions/industry-specific/js/main.js
@@ -0,0 +1,14 @@
+var walkDog = prompt("How many times a day do you walk your dog");
+if (Number(walkDog) > 4){
+    console.log("your pup is happy!");
+} else {
+    console.log("you are a bad pet owner");
+}
+
+
+function Name() {
+    var firstName = document.getElementById("fname").value;
+    var lastName = document.getElementById("lname").value;
+    console.log(firstName);
+    console.log(lastName);
+}

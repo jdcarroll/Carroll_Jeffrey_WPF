d3030e729914284a66fc2cfe5e01f0ec2f301720
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-11 10:53:27 -0400

    adding comments

--- assignment_expressions/personal/js/main.js
+++ assignment_expressions/personal/js/main.js
@@ -6,9 +6,16 @@ console.log("You really should be waling your dog" + response + "this many times
 
 
 
-function Name() {
-    var firstName = document.getElementById("fname").value;
-    var lastName = document.getElementById("lname").value;
-    console.log(firstName);
-    console.log(lastName);
+function Name() { // This is a function that I was testing the ability of , functions, but more so grabbing
+// elements from the DOM or Document Object Model which essentially means the html document
+    var firstName = document.getElementById("fname").value;// so for javascript you start with the document and
+        // at that point with document.getElementById() you are searching in the document for an html element with an
+        // id that equals what in the () in this case it is search for a string called fname and putting it into
+        // a variable called firstName
+    var lastName = document.getElementById("lname").value;// so for javascript you start with the document and
+        // at that point with document.getElementById() you are searching in the document for an html element with an
+        // id that equals what in the () in this case it is search for a string called lname and putting it into
+        // a variable called lastName
+    console.log(firstName); // I am then taking the variable firstName and printing out the users input onto the console
+    console.log(lastName); // I am then taking the variable lastName and printing out the users input onto the console
 }
--- assignment_expressions/wacky/js/main.js
+++ assignment_expressions/wacky/js/main.js
@@ -1,8 +1,18 @@
 var punch = prompt("You are in a fight how many times do you want to punch your opponent?");
 var sure = prompt("Are you sure?");
-    if (sure == yes || sure == y){
-        prompt("You are a where you are fighting Mike Tyson Right?" + "I cant believe you tried to punch Mike Tyson" + punch +
-        " many times.");
+var mikeTysonPunches = punch * 10000;
+    if (sure == "yes" || sure == "y"){
+        alert("You are a where you are fighting Mike Tyson Right? " + "I can't believe you tried to punch Mike Tyson " + punch +
+        " times.");
+        console.log("You are a where you are fighting Mike Tyson Right?" + "I can't believe you tried to punch Mike Tyson " + punch +
+            "  times.")
     } else {
-        alert("your not as dumb because your opponent is Mike Tyson but you loose anyway");
+        alert("your not as dumb because your opponent is Mike Tyson but you loose anyway. I still can't believe " +
+            "you said you would punch Mike Tyson " + punch + "times." );
+        console.log("your not as dumb because your opponent is Mike Tyson but you loose anyway. I still can't believe " +
+            "you said you would punch Mike Tyson " + punch + "times.");
     }
+console.log("You may have attempted to punch Mike Tyson " + punch + " times. However the truth is you really only punched " +
+    "him 1 time and he punched you " + mikeTysonPunches + " times but he only need 1 punch");
+alert("You may have attempted to punch Mike Tyson " + punch + " times. However the truth is you really only punched " +
+    "him 1 time and he punched you " + mikeTysonPunches + " times but he only need 1 punch");

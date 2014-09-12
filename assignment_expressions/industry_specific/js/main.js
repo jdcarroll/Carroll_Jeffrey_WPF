e0318e27ad77d0ff79c6b1a21e4e124c7704e715
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-12 15:27:59 -0400

    Updating for specifics of assignment

--- assignment_expressions/industry_specific/js/main.js
+++ assignment_expressions/industry_specific/js/main.js
@@ -1,10 +1,15 @@
 // Assignment Expressions by Jeff Carroll on 9-10-14
 
+var calculationArray = ["PDF ", "JPG ", "GIF ", "EPS ", "PNG "];
+
+var imagetype = prompt("please choose from the following the type of file: " + calculationArray)
+
 var imgHeight = Number(prompt("What is the height of your image?")); // I am asking the user for the height of the image.
 
 var imgWidth = Number(prompt("What is the width of your image?")); // I am asking for the width of the image
 
-var screenWidth = window.innerWidth;// I am using this to calculate the window size.
+var screenWidth = window.innerWidth;// I am using this to calculate the window size. I don't directly ask the user for this
+// but it is gathered up when the window is loaded
 
 var across = Math.floor(screenWidth / imgWidth);// I am attempting to calculate how many times the image goes across the screen
 
@@ -14,6 +19,7 @@ var down = Math.floor(screenHeight / imgHeight);// Im attempting to calculate ho
 
 var screenSize = screenHeight * screenWidth; // This is where the actual calculation happens.
 
-console.log("your Screen Size equals: " + screenSize + "px and you can fit the image across the " +
-    "screen " + across + " times.  You can fit the img down the screen " + down + " times");
-    /* I am echoing out the results of the calculation to the console.*/
+console.log("Your screen width is equal to " + screenWidth + "px and your screen height is equal to " + screenHeight + "px Making " +
+    "your total Screen Size equal to: " + screenSize + "px^2 and you can fit the " + imagetype + " image across the " + "screen " + across + " times.  " +
+    "You can fit the " + imagetype + " image down the screen " + down + " times"); /* I am echoing out the results of the calculation
+    to the console.*/
--- assignment_expressions/wacky/js/main.js
+++ assignment_expressions/wacky/js/main.js
@@ -1,5 +1,9 @@
 // Assignment Expressions by Jeff Carroll on 9-10-14
 
+var name = prompt("What is your name?"); // I am asking the user his name to call him specifically and personally through
+// the process
+var weapon = ["Boxing Gloves", "Brass Knuckles", "nunchucks"];
+var protection = prompt("What would you like to use for protection? " + weapon);
 var punch = Number(prompt("You are in a fight how many times do you want to punch your opponent?")); // I am asking for
 // a number
 var sure = String(prompt("Are you sure?")); // I m asking for a string because they are about to fight mike tyson and I
@@ -14,8 +18,8 @@ var weak = punch / punch; // This here is a reality check if you think you are g
             "Tyson " + punch + " times."); // I am trying illustrate the importance of non violence here I thought using
             // Mike Tyson as an example would be good to illustrate this
         console.log("You are a where you are fighting Mike Tyson Right?" + "I can't believe you tried to punch Mike " +
-            "Tyson " + punch + " times.");// I am trying illustrate the importance of non violence here I thought using
-        // Mike Tyson as an example would be good to illustrate this
+            "Tyson " + punch + " times and with " + protection + " non the less.");// I am trying illustrate the importance
+            // of non violence here I thought using Mike Tyson as an example would be good to illustrate this
     } else {
         alert("your not as dumb because your opponent is Mike Tyson but you loose anyway. I still can't believe " +
             "you said you would punch Mike Tyson " + punch + " times." ); // here is where I respond when they say that
@@ -27,7 +31,7 @@ var weak = punch / punch; // This here is a reality check if you think you are g
 console.log("You may have attempted to punch Mike Tyson " + punch + " times. However the truth is you really only got " +
     "him " + weak + " time and he punched you " + mikeTysonPunches + " times but he only need 1 punch"); // This is the
     // overall summary of what has happened during this process
-alert("You may have attempted to punch Mike Tyson " + punch + " times. However the truth is you ou really only got " + weak +
+alert("You may have attempted to punch Mike Tyson " + punch + " times. However the truth is you really only got " + weak +
     "him time and he punched you " + mikeTysonPunches + " times but he only need 1 punch"); // This is the
 // overall summary of what has happened during this process
 

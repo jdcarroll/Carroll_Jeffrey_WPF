711867adcef79671acdf99eaa76ac2a0d62ebabb
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-12 22:25:42 -0400

    just because

--- Conditionals_worksheet/homework/js/main.js
+++ Conditionals_worksheet/homework/js/main.js
@@ -1,3 +1,19 @@
-Function Temp() {
+function Temp() {
+    var temperature = Number(document.getElementById("Temp"));
+    var fahrenheit = document.getElementById("F");
+    fahrenheit.checked = true;
+    var celsius = document.getElementById("C");
+    celsius.checked = true;
+    var celsiusFromF;
 
+
+    if (fahrenheit) {
+        console.log("Fahrenheit!");
+        celsiusFromF = (temperature - 32)*(5/9);
+        console.log(celsiusFromF);
+    }else if (celsius) {
+        console.log("Celsius!");
+        var fahrenheitFromC = (temperature *(5/9) + 32);
+        console.log(fahrenheitFromC);
+    }
 }

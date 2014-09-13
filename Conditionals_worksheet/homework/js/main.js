04b2f1b502ace0aa05265c536191a4da328e2e31
jdcarroll <jeffreycarroll@jdc-mac.corp.idtus.com>
2014-09-13 00:24:14 -0400

    completed

--- Conditionals_worksheet/homework/js/main.js
+++ Conditionals_worksheet/homework/js/main.js
@@ -15,3 +15,48 @@ function Temp() {
         console.log(celsiusFromF);
     }
 }
+
+function login() {
+    var userName = document.getElementById("userName").value;
+    var userPassword = document.getElementById("userPassword").value;
+    var dataBaseUser = "superSecretUser";
+    var dataBasePassword = "superSecretPassword";
+
+    console.log(userName);
+    console.log(userPassword);
+    if (userName === dataBaseUser && userPassword === dataBasePassword) {
+        document.getElementById("message").innerHTML = "Welcome! You Login Successfully";
+    }else if (userName === dataBaseUser)  {
+        if (userPassword != dataBasePassword){
+            document.getElementById("message").innerHTML = "Hey Buddy you got your password wrong";
+        }
+    }else {
+        document.getElementById("message").innerHTML = "You fail";
+    }
+
+}
+
+function tire() {
+    var driverTire = document.getElementById("tired").value;
+    var passengerTire = document.getElementById("tirep").value;
+    var rearDriverTire = document.getElementById("tirerd").value;
+    var rearPassengerTire = document.getElementById("tirerp").value;
+    var standardPsi = 38;
+
+    console.log(driverTire, passengerTire, rearDriverTire, rearPassengerTire);
+
+    if ((driverTire === passengerTire) && (rearDriverTire === rearPassengerTire)) {
+        document.getElementById("pressure").innerHTML = "Your Tires are good to go!!";
+    }else if ((driverTire === passengerTire) && (rearDriverTire != rearPassengerTire)){
+        document.getElementById("pressure").innerHTML = "You should get air in your back two tires to " +
+            "match " + standardPsi +"psi";
+    } else if ((driverTire != passengerTire) && (rearDriverTire === rearPassengerTire)) {
+        document.getElementById("pressure").innerHTML = "You should get air in your front two tires to " +
+            "match " + standardPsi +"psi";
+    } else if ((driverTire != passengerTire) && (rearDriverTire != rearPassengerTire)) {
+        document.getElementById("pressure").innerHTML = "You should get air in all four of your tires to " +
+            "match " + standardPsi + "psi";
+    }
+
+}
+

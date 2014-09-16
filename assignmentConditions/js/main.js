cf2c69d7bfc87dfe0fae12d6cf53c389be93c997
jdcarroll <jdcarroll@fullsail.edu>
2014-09-16 09:43:22 -0400

    updates

--- assignmentConditions/js/main.js
+++ assignmentConditions/js/main.js
@@ -49,5 +49,54 @@ function wattage() {
     var watts = document.getElementById("wattage").value;
     var amps = document.getElementById("amps").value;
     var volts = document.getElementById("volts").value;
+    var blankArray = [];
 
+    if (watts == ""){
+        if ((amps != "") && (volts != "")){
+            var result = amps * volts;
+            document.getElementById("update").innerHTML = "Your wattage calculation: " + result.toFixed(2);
+        }else {
+            blankArray.push(" watts");
+            document.getElementById("update").innerHTML = "You did not enter anything for " + blankArray;
+        }
+    }
+    if (amps == "") {
+        if ((watts != "") && (volts != "")) {
+            var result = watts / volts;
+            document.getElementById("update").innerHTML = "Your amps calculation: " + result.toFixed(2);
+        } else {
+            blankArray.push(" amps");
+            document.getElementById("update").innerHTML = "You did not enter anything for " + blankArray;
+        }
+    }
+    if (volts == "") {
+        if ((watts != "") && (amps != "")) {
+            var result = Math.sqrt(watts * amps);
+            document.getElementById("update").innerHTML = "Your volts calculation: " + result.toFixed(2);
+        } else {
+            blankArray.push(" amps");
+            document.getElementById("update").innerHTML = "You did not enter anything for " + blankArray;
+        }
+    }
+
+}
+
+function calculateBandwidth() {
+    var bandwidth = document.getElementById("bandwidth").value;
+    var devices = document.getElementById("devices").value;
+
+
+}
+
+function populate(){
+    var activityList = ["Streaming Media", "Internet", "Wireless Home Network"];
+    var sel = document.getElementById("activity");
+    var output = "<option>";
+    for ( var i = 0; i < activityList.length; i++ ) {
+        var opt = document.createElement('option');
+        opt.innerHTML = activityList[i];
+        opt.value = activityList[i];
+        sel.appendChild(opt);
+    }
+    output += "</option>"
 }

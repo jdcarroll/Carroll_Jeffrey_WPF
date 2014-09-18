66754f88ec3c195ab1deee60eddd257a0523aeab
jdcarroll <jdcarroll@fullsail.edu>
2014-09-17 23:57:48 -0400

    update

--- assignmentConditions/js/balh.js
+++
@@ -1,8 +0,0 @@
-var a = prompt("first numerator please?");
-var b = prompt("first denominator please?");
-var c = prompt("second numerator please?");
-var d = prompt("second numerator please?");
-
-var result = a / b = c / d;
-
-console.log(result);
